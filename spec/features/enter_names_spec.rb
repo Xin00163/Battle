f5993f8546f316d8f4687df7db9fea449de45076
tom <tomallpress@hotmail.com>
2017-12-06 15:07:20 +0000

    added sessionand updated instance variable to sessions

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,11 +1,10 @@
 feature 'Testing enter names' do
 
-  scenario 'entering player ones name thn submitting saves the name of player one' do
+  scenario 'entering the players names then submitting brings us to the next page' do
     visit ('/')
     fill_in :player_1_name, :with => "Tom"
     fill_in :player_2_name, :with => 'Nikolas'
     click_button('Submit')
     expect(page).to have_content "Tom vs Nikolas"
   end
-
 end
